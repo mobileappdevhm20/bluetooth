abd5c1c56014cf9052860120eec28a02d089c698
David <haupenth@hm.edu>
2020-05-31 16:24:05 +0200

    code refractored

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,17 +1,17 @@
 
-y
-Iapp/src/main/java/github/com/mobileappdevhm20/bluetooth/start_fragment.kt,4\2\42bc6fd628da44a65de09fd0efffd0800788c4e3
-x
-Happ/src/main/java/github/com/mobileappdevhm20/bluetooth/chat_fragment.kt,c\d\cd747380d2c695dc5e95b9fd96fe02e8bc6d04c2
 P
  app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
 w
 Gapp/src/main/java/github/com/mobileappdevhm20/bluetooth/MainActivity.kt,f\8\f8b2448e44afe2d4087c46819fa338f38ff02058
-
-Oapp/src/main/java/github/com/mobileappdevhm20/bluetooth/BluetoothChatService.kt,7\f\7f3b145405390e33287fb82c93c7e9fbefb5e84f
-t
-Dapp/src/main/java/github/com/mobileappdevhm20/bluetooth/Constants.kt,e\e\eea6712f38ab76fc81b3c14d82b836cb6bde8081
 }
 Mapp/src/main/java/github/com/mobileappdevhm20/bluetooth/DeviceListActivity.kt,b\6\b68d03df4a21afe56154c964390f25f86bd10dfa
-€
-Papp/src/main/java/github/com/mobileappdevhm20/bluetooth/BluetoothChatFragment.kt,0\e\0e8d0b343b20c9197401b9dd835ac3ceb19f416f
+R
+"app/src/main/res/values/dimens.xml,c\3\c3c1811dec2dbad2deeafb9aad505ca9631da740
+R
+"app/src/main/res/values/styles.xml,f\7\f79cc2688e29fcbc286fda14ae07b27c9e83690f
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+Y
+)app/src/main/res/navigation/nav_graph.xml,b\a\baffce053da291d96fb575a09f2bbe9aaa9c4be9
+{
+Kapp/src/main/java/github/com/mobileappdevhm20/bluetooth/BluetoothService.kt,c\8\c84e77dc47565e0cb18e0e3b6712b505203aaadc
--- app/src/main/java/github/com/mobileappdevhm20/bluetooth/MainActivity.kt
+++ app/src/main/java/github/com/mobileappdevhm20/bluetooth/MainActivity.kt
@@ -1,16 +1,15 @@
 package github.com.mobileappdevhm20.bluetooth
 
-import android.app.PendingIntent.getActivity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
@@ -22,200 +21,58 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-
-
-        val rollButton: Button = findViewById(R.id.button5)
-        rollButton.text = "Let's Roll"
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-            if (bluetoothAdapter == null) {
-                // Device doesn't support Bluetooth
-                Toast.makeText(this, "Bluetooth not support", Toast.LENGTH_SHORT).show()
-
-            } else {
-                if (!bluetoothAdapter?.isEnabled) {
-                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                    startActivityForResult(enableBtIntent, 10)
-                    Toast.makeText(this, "Turned on", Toast.LENGTH_LONG).show();
-
-                } else {
-                    Toast.makeText(this, "Already on", Toast.LENGTH_LONG).show();
-
-                }
-            }
-        }
-        val rollButton1: Button = findViewById(R.id.button6)
-        rollButton1.setOnClickListener {
-            Toast.makeText(this, "button chool", Toast.LENGTH_SHORT).show()
-            if (bluetoothAdapter == null) {
-                // Device doesn't support Bluetooth
-                Toast.makeText(this, "Bluetooth not support", Toast.LENGTH_SHORT).show()
-
-            } else {
-                bluetoothAdapter.disable()
-                Toast.makeText(this, "Turned off", Toast.LENGTH_LONG).show();
-
-            }
-        }
-
-
-        val rollButton2: Button = findViewById(R.id.button7)
-        rollButton2.setOnClickListener {
-            Toast.makeText(this, "button discover", Toast.LENGTH_SHORT).show()
-            val discoverableIntent: Intent =
-                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-                    putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
-                }
-            startActivity(discoverableIntent)
-        }
-
-        val rollButton60: Button = findViewById(R.id.button)
-        rollButton60.setOnClickListener {
-            Toast.makeText(this, "bframgment", Toast.LENGTH_SHORT).show()
-            //s  val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-
-
-            // Launch the DeviceListActivity to see devices and do scan
-            val serverIntent = Intent(this, DeviceListActivity::class.java)
-            startActivityForResult(
-                serverIntent,
-                11
-            )
-
-
-        }
-
-
-        var pairedDevices1: MutableList<String> = ArrayList()
-        var listView: ListView = findViewById(R.id.listView)
-        val rollButton3: Button = findViewById(R.id.button2)
-        rollButton3.setOnClickListener {
-            Toast.makeText(this, "button discover", Toast.LENGTH_SHORT).show()
-            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
-            pairedDevices?.forEach { device ->
-                val deviceName = device.name
-                val deviceHardwareAddress = device.address // MAC address
-                Toast.makeText(
-                    this,
-                    "button discover $deviceName $deviceHardwareAddress",
-                    Toast.LENGTH_SHORT
-                ).show()
-                pairedDevices1.add(deviceName)
-
-            }
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, pairedDevices1)
-            listView.adapter = adapter
-
-        }
-
-
-        // Register for broadcasts when a device is discovered.
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
-        val rollButton4: Button = findViewById(R.id.button3)
-        rollButton4.setOnClickListener {
-            Toast.makeText(this, "button discover", Toast.LENGTH_SHORT).show()
-
-            if (bluetoothAdapter != null) {
-                bluetoothAdapter.startDiscovery()
+        if (bluetoothAdapter != null) {
+            val text: TextView = findViewById(R.id.label)
+            if (!bluetoothAdapter?.isEnabled) {
+                text.text = "on"
+            }else{
+                text.text = "off"
             }
         }
 
 
     }
-
-
-/*
-        private inner class ConnectedThread(private val mmSocket: BluetoothSocket) : Thread() {
-            private  val TAG = "MY_APP_DEBUG_TAG"
-
-            private val mmInStream: InputStream = mmSocket.inputStream
-            private val mmOutStream: OutputStream = mmSocket.outputStream
-            private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
-
-            override fun run() {
-                var numBytes: Int // bytes returned from read()
-
-                // Keep listening to the InputStream until an exception occurs.
-                while (true) {
-                    // Read from the InputStream.
-                    numBytes = try {
-                        mmInStream.read(mmBuffer)
-                    } catch (e: IOException) {
-                        Log.d(TAG, "Input stream was disconnected", e)
-                        break
-                    }
-
-                    // Send the obtained bytes to the UI activity.
-                    val readMsg = handler.obtainMessage(
-                        MESSAGE_READ, numBytes, -1,
-                        mmBuffer)
-                    readMsg.sendToTarget()
-                }
-            }
-
-            // Call this from the main activity to send data to the remote device.
-            fun write(bytes: ByteArray) {
-                try {
-                    mmOutStream.write(bytes)
-                } catch (e: IOException) {
-                    Log.e(TAG, "Error occurred when sending data", e)
-
-                    // Send a failure message back to the activity.
-                    val writeErrorMsg = handler.obtainMessage(MESSAGE_TOAST)
-                    val bundle = Bundle().apply {
-                        putString("toast", "Couldn't send data to the other device")
-                    }
-                    writeErrorMsg.data = bundle
-                    handler.sendMessage(writeErrorMsg)
-                    return
-                }
-
-                // Share the sent message with the UI activity.
-                val writtenMsg = handler.obtainMessage(
-                    MESSAGE_WRITE, -1, -1, mmBuffer)
-                writtenMsg.sendToTarget()
-            }
-
-            // Call this method from the main activity to shut down the connection.
-            fun cancel() {
-                try {
-                    mmSocket.close()
-                } catch (e: IOException) {
-                    Log.e(TAG, "Could not close the connect socket", e)
-                }
-
+//Turn on Bluetooth
+    fun on(view: View) {
+        if (!bluetoothAdapter?.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 10)
+            Toast.makeText(this, "Turned on", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+//Turn off Bluetooth
+    fun off(view: View) {
+        bluetoothAdapter.disable()
+        Toast.makeText(this, "Turned off", Toast.LENGTH_LONG).show();
 
     }
-*/
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private val receiver = object : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            val action: String? = intent.action
-            when (action) {
-                BluetoothDevice.ACTION_FOUND -> {
-                    // Discovery has found a device. Get the BluetoothDevice
-                    // object and its info from the Intent.
-                    val device: BluetoothDevice =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC address
-                    val text: TextView = findViewById(R.id.textView)
-
-                    text.text = "deviceName"
-                }
+//Make the Device Discoverable. Other devices can find the device and can connect
+    fun makeDiscoverable(view: View) {
+        Toast.makeText(this, "button discover", Toast.LENGTH_SHORT).show()
+        val discoverableIntent: Intent =
+            Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
             }
-        }
+        startActivity(discoverableIntent)
+    }
+//Show all Paired Devices or scan for new Devices
+    fun select(view: View) {
+        Toast.makeText(this, "bframgment", Toast.LENGTH_SHORT).show()
+
+        // Launch the DeviceListActivity to see devices and do scan
+        val serverIntent = Intent(this, DeviceListActivity::class.java)
+        startActivityForResult(
+            serverIntent,
+            11
+        )
     }
 
 
     override fun onStart() {
         super.onStart()
-        if (bluetoothAdapter == null) {
-            return
-        }
+
         // If BT is not on, request that it be enabled.
         // setupChat() will then be called during onActivityResult
         if (!bluetoothAdapter.isEnabled()) {
@@ -230,13 +87,6 @@ class MainActivity : AppCompatActivity() {
         var BluetoothService = MyBluetoothService(mHandler)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver)
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -266,7 +116,7 @@ class MainActivity : AppCompatActivity() {
     private fun abc(a: String) {
         Log.i("Me1:", "hjkhjh  $a")
 
-        val text: TextView = findViewById(R.id.label)
+        val text: TextView = findViewById(R.id.label1)
 
         text.text = "$a"
         a.substring(0, 10)
